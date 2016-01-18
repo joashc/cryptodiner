70f0a9735773c1ed6f216a919b99d725a351f721
joashc <joash.is@gmail.com>
2016-01-18 18:40:19 +1100

    Add comments

--- dcpeer.hs
+++ dcpeer.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE DeriveFunctor, TemplateHaskell #-}
-
 import Control.Monad.State
 import Control.Monad.Free
 import Control.Lens
 
+------------------------------------------------------------
+-- Define our data types
+------------------------------------------------------------
 data DcPeerOperator next =
     PeerConnect String next
   | GetPeers ([String] -> next)
@@ -18,10 +20,14 @@ data ServerState = SS {
   _registeredPeers :: [PeerState]
 } deriving (Show)
 
-makeLenses ''ServerState
-
 data Message = PeerData | Stream deriving (Show)
 
+-- Automagic some lenses with TH
+makeLenses ''ServerState
+
+------------------------------------------------------------
+-- Define our DSL for the server
+------------------------------------------------------------
 data DcServerOperator next =
     InitServer next
   | GetMessage (Message -> next)
@@ -35,6 +41,9 @@ data DcServerOperator next =
 -- The free monad over DcServerOperator
 type DcServer = Free DcServerOperator
 
+------------------------------------------------------------
+-- A bunch of boilerplate functions for the server operators
+------------------------------------------------------------
 initServer :: DcServer ()
 initServer = liftF $ InitServer ()
 
@@ -56,8 +65,39 @@ getServerState = liftF $ GetServerState id
 pause :: DcServer ()
 pause = liftF $ Pause ()
 
+------------------------------------------------------------
+-- Let's get the semantics of the server written in the DSL
+------------------------------------------------------------
+serverProg :: DcServer ()
+serverProg = do
+  initServer
+  forever $ getMessage >>= messageHandler
+
+messageHandler :: Message -> DcServer ()
+messageHandler PeerData = do
+  addPeer $ PS []
+  ss <- getServerState
+  if ss^.numPeers <= ss^.registeredPeers.to length
+  then pause
+  else sayString "Waiting for more peers!"
+messageHandler Stream = sayString "Stream"
+
+
+------------------------------------------------------------
+-- Let's write an interpreter that does all the nasty IO stuff
+------------------------------------------------------------
+
+-- Here's the type our serverIO interpreter is going to map the free monad to
 type DcServerIO = StateT ServerState IO
 
+{- | An IO interpreter for the DcServer free monad
+  We write the interpreter with the type:
+  > f (m a) -> m a
+  This is so we can use:
+  > iterM :: (Monad m, Functor f) => (f (m a) -> ma) -> Free f a -> m a
+  instead of writing the interpreter directly in Free f a and having to type Pure
+  and Free all over the place
+-}
 serverIO :: DcServerOperator (DcServerIO next) -> DcServerIO next
 serverIO (InitServer next) = do
   liftIO $ putStrLn "Enter peers:"
@@ -71,9 +111,7 @@ serverIO (SayString s next) = do
 serverIO (GetMessage next) = do
   ss <- get
   next PeerData
-serverIO (GetServerState next) = do
-  ss <- get
-  next ss
+serverIO (GetServerState next) = get >>= next
 serverIO (AddPeer peer next) = do
   liftIO $ putStrLn "Adding peer"
   registeredPeers %= (:) peer
@@ -83,20 +121,7 @@ serverIO (Pause next) = do
   x <- liftIO getLine
   next
 
-messageHandler :: Message -> DcServer ()
-messageHandler PeerData = do
-  addPeer $ PS []
-  ss <- getServerState
-  if ss^.numPeers <= ss^.registeredPeers.to length
-  then pause
-  else sayString "Waiting for more peers!"
-messageHandler Stream = sayString "Stream"
-
-serverProg :: DcServer ()
-serverProg = do
-  initServer
-  forever $ getMessage >>= messageHandler
-
+-- This 
 serve :: DcServer a -> DcServerIO a
 serve = iterM serverIO
 
